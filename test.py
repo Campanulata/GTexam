2b14f52768ae77c325943f44ccc9d10d685080a7
tylor <tylor@Mac-mini.local>
2020-07-11 14:11:20 +0800

    全国3

--- test.py
+++ test.py
@@ -7,23 +7,26 @@ from docx.shared import Pt
 from docx.shared import RGBColor
 from docx.shared import Inches
 import re
+def replaceStr(allStr,str1,str2):
+    allStr=allStr.replace(str1,str2)
 def delQuad(allStr):
     return allStr.replace(' ','')
 def latex(allStr):
     result=re.findall(r'[^\u4E00-\u9FA5]+',allStr)
     result=list(set(result))
     for j in range(len(result)):
-        if len(result[j])<3:
+        if len(result[j])<3 :
             result[j]='willDel'
-        # if '\n' in result[j]:
-        #     result[j]='willDel'
+        if r'includegraphics' in result[j] or r'image' in result[j]:
+            result[j]='willDel'
     while 'willDel' in result:
         result.remove('willDel')
     for m in range(0,len(result)-1):#从大到小排序
         for p in range(0,len(result)-1-m):
             if len(result[p])<len(result[p+1]):
                 result[p],result[p+1]=result[p+1],result[p]
-    for j in ['(1)','(2)','(3)','(4)','(a)','(b)']:#移除不需要加$的字符
+    #移除不需要加$的字符
+    for j in ['(1)','(2)','(3)','(4)','(a)','(b)',r'\begin{center}\includegraphics[',r'{img/image1.png}\end{center}',r'\begin{center}\includegraphics[]{img/image1.png}\end{center}',r'.\begin{center}\includegraphics[',r'{img/image1.png}\end{center}A.',r'?\begin{center}\includegraphics[']:
         if j in result:
             result.remove(j)
     for j in range(0,len(result)):#替换 加$
@@ -35,21 +38,32 @@ def latex(allStr):
 
 document = docx.Document('/Users/tylor/Desktop/1.docx') 
 par = document.paragraphs
-imgCount=0
-for i in par:
-    t=i.text.count('imgLaTeX')
-    for j in ['\key{}','。','、',',','(1)','(2)','(3)','(4)','( 1 )','( 2 )','( 3 )','( 4 )',':','(i)','(ii)','(',')',']']:
-        i.text=i.text.replace(j,'答案'+j+'答案')
-    
-
 # 导出图片到img文件夹
+imgCount=0
 for rel in document.part._rels:
     rel = document.part._rels[rel]               #获得资源
     if "image" not in rel.target_ref:
         continue
     imgName = re.findall("/(.*)",rel.target_ref)[0]
+    imgCount+=1
     with open('img' + "/" + imgName,"wb") as f:
         f.write(rel.target_part.blob)
+#加图
+for j in range(1,imgCount+1):#imagej.png
+    for i in par:
+        if i.text=='':
+            i.text=r'\begin{center}'+r'\includegraphics[]{img/image'+str(j)+r'.png}'+r'\end{center}'
+            break
+        continue
+for i in par:
+    i.text=i.text.replace(r'∠',r'\angle')
+    i.text=i.text.replace(r'ⅱ',r'ii')
+    
+    for j in ['\key{}','。','、',',',':','(i)','(ii)','(',')',']','，']:
+        i.text=i.text.replace(j,'答案'+j+'答案')
+    
+
+
 # 替换空行，此方法库中没实现
 for i in range(len(par)):#清空非必要信息
     if par[i].text.find("百校翻联监")>=0 or par[i].text.find("注意事项")>=0:
@@ -102,10 +116,7 @@ for i in range(1, choiceNum + 1):  # 生成选择题
     choice1=delQuad(choice1)
     choice1=latex(choice1)
     choice1=r'\question[6] '+choice1
-    while i in listWithImg:
-        choice1+='\n'+r'\begin{center}'+'\n'+r'\includegraphics[]{img/image'+str(imgNo)+r'.png}'+'\n'+r'\end{center}'+'\n'
-        imgNo+=1
-        listWithImg.remove(i)
+
     # 选项
     choice2 = ''  
     choiceA = listABCD[i]
@@ -140,10 +151,7 @@ for i in range(choiceNum + 1, maxNum + 1):  # 生成选择题
     choice1=delQuad(choice1)
     choice1=latex(choice1)
     choice1 = r'\question[6]'+choice1
-    while i in listWithImg:
-        choice1+='\n'+r'\begin{center}'+'\n'+r'\includegraphics[]{img/image'+str(imgNo)+r'.png}'+'\n'+r'\end{center}'+'\n'
-        imgNo+=1
-        listWithImg.remove(i)
+
     unChoiceAll+=choice1+'\n'
 # 去除空格
 
@@ -151,7 +159,7 @@ unChoiceAll=unChoiceAll.replace(r'$$','')
 
 
 def latexQuad(strAll):
-    for i in [r'\times',r'\pi',r'\Delta',r'\rightarrow']:
+    for i in [r'\times',r'\pi',r'\Delta',r'\rightarrow',r'\angle',r'\leqslant']:
         strAll=strAll.replace(i,i+' ')
     return strAll
 
@@ -166,11 +174,12 @@ unChoiceAll=unChoiceAll.replace(r'\question[6]',r'\question[6] ')
 def delHeadFoot(strAll,key):
     return strAll.replace('答案'+key+'答案',key)
 #中文保护 替换
-for i in ['。','、',',','(1)','(2)','(3)','(4)',':','(i)','(ii)','(',')',']']:
+for i in ['。','、',',',':','(i)','(ii)','(',')',']','，']:
     choiceAll=delHeadFoot(choiceAll,i)
     unChoiceAll=delHeadFoot(unChoiceAll,i)
 choiceAll=choiceAll.replace(r'答案$\key{}$答案',r'\key{}')
 unChoiceAll=unChoiceAll.replace(r'答案$\key{}$答案',r'\key{}')
+
 #sub
 dictSub={'1':'','2':'','3':'','4':'','5':'','6':'','7':''}
 for i in dictSub:#生产sub标题
